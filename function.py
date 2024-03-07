1b4fddc8bfd06c15a63c207c7f8057dcd5742269
Felipe Vargas <felipevargas.bz@gmail.com>
2024-03-07 00:09:55 -0500

    first commit

---
+++ function.py
@@ -0,0 +1,25 @@
+from PIL import Image
+from io import BytesIO
+
+
+def apply_frame(photo, frame_name):
+    frame_path = f"./frames/{frame_name.value}"
+
+    photo_img = Image.open(photo)
+
+    frame_img = Image.open(frame_path)
+
+    depth_width, depth_height = photo_img.size
+
+    frame_img = frame_img.resize((depth_width, depth_height))
+
+    photo_img = photo_img.convert('RGBA')
+
+    result_img = Image.alpha_composite(photo_img, frame_img)
+
+    result_img = result_img.convert('RGB')
+
+    result_bytesio = BytesIO()
+    result_img.save(result_bytesio, format='JPEG')
+
+    return result_bytesio.getvalue()
---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.responses import StreamingResponse
+from function import apply_frame
+from io import BytesIO
+from enum import Enum
+import os
+
+app = FastAPI(description="Usa el marco tu team en Facebook", summary="Elige tu Team en Facebook")
+
+
+class Frames(str, Enum):
+    natty = "natty.png"
+    caro = "caro.png"
+
+
+@app.post("/elegir/marco", )
+async def overlay_photo(marco: Frames, tu_foto_del_perfil: UploadFile = File(...)):
+    frame_name = marco.value
+    photo_content = await tu_foto_del_perfil.read()
+
+    result = apply_frame(BytesIO(photo_content), marco)
+
+    return StreamingResponse(BytesIO(result), media_type="image/jpeg", headers={"Content-Disposition": f"attachment; filename={frame_name}_overlay.jpg"})
+
+
+@app.post("/subir_marco")
+async def subir_frame(password: str, frame_name: str, marco: UploadFile = File(...)):
+    if password != "<PASSWORD>":
+        return JSONResponse
+    ruta_marco = os.path.join("./frames", f"{frame_name}.png")
+
+    try:
+        with open(ruta_marco, "wb") as buffer:
+            buffer.write(marco.file.read())
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al subir el marco: {str(e)}")
+
+    return JSONResponse(content={"mensaje": f"Marco {frame_name} subido exitosamente"})
